5ffb7550debcd23f707ecd5f897a3304be83fc67
Felipe Martins <ffcm@outlook.com>
2022-03-06 17:13:00 +1300

    converts project to use Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Game from '../src/lib/Game'
+
+const Home: NextPage = () => {
+  return <Game />
+}
+
+export default Home

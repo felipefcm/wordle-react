7070cc76b4ece67857acab17824b40ba81b5e653
Felipe Martins <ffcm@outlook.com>
2022-03-27 22:29:35 +1300

    makes keyboard letter cells background show correct state

--- src/common/MatchState.ts
+++ src/common/MatchState.ts
@@ -16,8 +16,40 @@ class MatchState {
 		return letterState || LetterState.UNKNOWN
 	}
 
+	getKeyboardLetterState(letter: string) {
+		return this.keyboardLettersState[letter.toUpperCase()] || LetterState.UNKNOWN
+	}
+
 	parseResult(attemptIndex: number, results: AttemptResult[]) {
 		this.attemptsState[attemptIndex] = results
+		this.updateKeyboardLetterState()
+		console.log(`NEW KB STATE`, this.keyboardLettersState)
+	}
+
+	private updateKeyboardLetterState() {
+
+		for (const attemptState in this.attemptsState) {
+			for (const letterState of this.attemptsState[attemptState]) {
+
+				const [letter, state] = letterState
+				const currentState = this.keyboardLettersState[letter]
+
+				if (state === LetterState.CORRECT) {
+					this.keyboardLettersState[letter] = LetterState.CORRECT
+					continue
+				}
+
+				if (state === LetterState.INCORRECT_POSITION && currentState !== LetterState.CORRECT) {
+					this.keyboardLettersState[letter] = LetterState.INCORRECT_POSITION
+					continue
+				}
+
+				if (state === LetterState.NOT_PRESENT && (!currentState || currentState === LetterState.UNKNOWN)) {
+					this.keyboardLettersState[letter] = LetterState.NOT_PRESENT
+					continue
+				}
+			}
+		}
 	}
 }
 

4055e694555c719fa4b2c6d370370f9c7b7ac4a6
Felipe Martins <ffcm@outlook.com>
2022-04-03 00:12:16 +1300

    adds action to deploy infra

--- projects/infra/stacks/WordleStack.ts
+++ projects/infra/stacks/WordleStack.ts
@@ -1,6 +1,6 @@
 import * as sst from '@serverless-stack/resources'
 
-export default class MyStack extends sst.Stack {
+export default class WordleStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props)
   }

34d77ac00e6b09af176076375efde662b12b5eba
Felipe Martins <ffcm@outlook.com>
2022-04-03 21:32:35 +1200

    adds proper words table config

--- projects/infra/stacks/WordleStack.ts
+++ projects/infra/stacks/WordleStack.ts
@@ -6,14 +6,14 @@ export default class WordleStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props)
 
-    // new dynamodb.Table(this, 'words', {
-    //   tableName: 'words',
-    //   readCapacity: 1,
-    //   writeCapacity: 1,
-    //   partitionKey: {
-    //     name: 'word',
-    //     type: dynamodb.AttributeType.STRING
-    //   },
-    // })
+    new dynamodb.Table(this, 'words', {
+      tableName: 'words',
+      readCapacity: 1,
+      writeCapacity: 1,
+      partitionKey: {
+        name: 'word',
+        type: dynamodb.AttributeType.STRING
+      },
+    })
   }
 }

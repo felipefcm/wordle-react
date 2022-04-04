b115f45a1d0bb7b09598d0a8c7712b31b0b62b51
felipe <felipefcm@users.noreply.github.com>
2022-04-04 23:53:04 +1200

    Validate incoming words (#13)

--- projects/game/pages/api/daily.ts
+++ projects/game/pages/api/daily.ts
@@ -3,15 +3,21 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { AttemptResult } from '@common/MatchState'
 import { getAttemptResult } from '@server/wordle'
+import { wordIsValid } from '@server/db'
 
 const dailyWordHandler = async (req: NextApiRequest, res: NextApiResponse<AttemptResult[] | { error: string }>) => {
 	const attempt = req.body.attempt as string
-	const testWord = req.body.testWord as string
 
-	const word = testWord || 'APPLE'
+	const isValid = await wordIsValid(attempt)
+	if (!isValid) {
+		res.json({ error: 'InvalidWord' })
+		return
+	}
+
+	const word = 'APPLE'
 
 	if (!attempt || attempt.length !== 5) {
-		res.status(400).json({ error: 'Invalid attempt' })
+		res.status(400).json({ error: 'InvalidAttempt' })
 		return
 	}
 
